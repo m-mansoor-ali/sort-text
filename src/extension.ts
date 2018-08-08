d6eb7fccd9d4a273fed21cb07967e167b9d9b0b6
Mansoor Ali <m.mansoor.ali@outlook.com>
2018-08-08 12:36:41 +0500

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import {Utilities} from './Utilities';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,79 +17,21 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
 
-    let disposableSortTextAsc = vscode.commands.registerCommand('extension.sortTextAsc', () => {
-        // The code you place here will be executed every time your command is executed
+    let commands = [
+        vscode.commands.registerCommand('sortText.sortText', Utilities.Sorter.sortNormal),
+        vscode.commands.registerCommand('sortText.sortTextReverse', Utilities.Sorter.sortReverse),
+        vscode.commands.registerCommand('sortText.sortTextCaseInsensitive', Utilities.Sorter.sortCaseInsensitive),
+        vscode.commands.registerCommand('sortText.sortTextLineLength', Utilities.Sorter.sortLineLength),
+        vscode.commands.registerCommand('sortText.sortTextLineLengthReverse', Utilities.Sorter.sortLineLengthReverse),
+        vscode.commands.registerCommand('sortText.sortTextNumeric', Utilities.Sorter.sortNumeric),
+        vscode.commands.registerCommand('sortText.sortTextUnique', Utilities.Sorter.sortUnique),
+        vscode.commands.registerCommand('sortText.sortTextShuffle', Utilities.Sorter.sortShuffle)
+    ];
 
-        // Display a message box to the user
-        //vscode.window.showInformationMessage('Hello World!');
+    commands.forEach(command => context.subscriptions.push(command));
 
-        let editor = vscode.window.activeTextEditor;
-        if (editor) {
-            sortTextAsc(editor, 'asc');
-        }
-
-    });
-
-    let disposableSortTextDesc = vscode.commands.registerCommand('extension.sortTextDesc', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        //vscode.window.showInformationMessage('Hello World!');
-
-        let editor = vscode.window.activeTextEditor;
-        if (editor) {
-            sortTextAsc(editor, 'desc');
-        }
-
-    });
-
-    context.subscriptions.push(disposableSortTextAsc);
-    context.subscriptions.push(disposableSortTextDesc);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-}
-
-function sortTextAsc(editor: vscode.TextEditor | undefined, order: string) {
-    if (editor) {
-
-        let lines = getAllLinesFromEditor(editor);
-
-        lines.sort();
-
-        if (order.toLowerCase() == "desc") {
-            lines.reverse();
-        }
-
-        let sortedText = "";
-        lines.forEach(line => {
-            sortedText = sortedText + line + "\n";
-        });
-        
-        replaceAllTextInEditor(editor, sortedText.trim());
-
-    }
-}
-
-function getAllLinesFromEditor(editor: vscode.TextEditor): Array<string> {
-
-    let lines = new Array();
-
-    for (let i = 0; i < editor.document.lineCount; i++) {
-        lines[i] = editor.document.lineAt(i).text;
-    }
-    return lines;
-
-}
-
-function replaceAllTextInEditor(editor: vscode.TextEditor, text: string) {
-    let firstLine = editor.document.lineAt(0);
-    let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-    let textRange = new vscode.Range(0,
-        firstLine.range.start.character,
-        editor.document.lineCount - 1,
-        lastLine.range.end.character);
-
-    editor.edit(editBuilder => editBuilder.replace(textRange, text));
 }
